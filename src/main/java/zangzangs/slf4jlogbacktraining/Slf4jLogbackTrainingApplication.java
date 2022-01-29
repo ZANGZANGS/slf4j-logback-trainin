e36b1b50599dd566f726a7e6998994aaed20cd9e
zangzangs <kimjangh94@gmail.com>
2022-01-29 16:48:23 +0900

    Initial commit

---
+++ src/main/java/zangzangs/slf4jlogbacktraining/Slf4jLogbackTrainingApplication.java
@@ -0,0 +1,13 @@
+package zangzangs.slf4jlogbacktraining;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Slf4jLogbackTrainingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Slf4jLogbackTrainingApplication.class, args);
+    }
+
+}
