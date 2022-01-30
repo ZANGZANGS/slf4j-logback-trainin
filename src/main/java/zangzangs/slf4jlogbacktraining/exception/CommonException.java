8e815ef6c541db24155f60bb87b178f7a5532513
zangzangs <kimjangh94@gmail.com>
2022-01-30 15:08:45 +0900

    feat: exception 최초생성

---
+++ src/main/java/zangzangs/slf4jlogbacktraining/exception/CommonException.java
@@ -0,0 +1,7 @@
+package zangzangs.slf4jlogbacktraining.exception;
+
+public class CommonException extends RuntimeException {
+    public CommonException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/zangzangs/slf4jlogbacktraining/exception/NotFoundException.java
@@ -0,0 +1,7 @@
+package zangzangs.slf4jlogbacktraining.exception;
+
+public class NotFoundException extends CommonException {
+    public NotFoundException(String message) {
+        super(message);
+    }
+}
